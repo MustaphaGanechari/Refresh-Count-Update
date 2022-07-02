0f972d286ce0677c67deb985311dd1f1dbefdb74
Mustapha Ganechari <mustafaganechari@gmail.com>
2022-07-03 02:50:11 +0530

    Intital Commit

---
+++ RefreshCountUpdate/index.js
@@ -0,0 +1,31 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter)=>{
+
+    counter.innerHTML=0;
+
+    const updateCounter = () =>{
+
+        const targetcount = +counter.getAttribute('data-target');
+
+        const startingCount = Number(counter.innerHTML);
+
+        const incr = targetcount/100;
+
+        if (startingCount < targetcount) {
+
+            counter.innerHTML = `${startingCount + incr}`;
+
+            setTimeout(updateCounter,10);
+            
+        }
+        else{
+            counter.innerHTML=targetcount;
+        }
+
+    }
+
+    updateCounter();
+})
+
+
